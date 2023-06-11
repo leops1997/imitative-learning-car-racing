e29a198202836c2ec810ebdcd11375f93f572218
leops1997 <leops1997@gmail.com>
2023-06-10 22:41:13 -0300

    Need to normalize "y" between 0 and 1 in "train"

--- cnn.py
+++ cnn.py
@@ -8,46 +8,51 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.pool = nn.MaxPool2d(2 ,2)
+        self.pool = nn.MaxPool2d(kernel_size=(2,2))
         self.conv1 = nn.Conv2d(3, 32, kernel_size=(8,8), stride=4)
         self.act1 = nn.ReLU()
         self.conv2 = nn.Conv2d(32, 64, kernel_size=(4,4), stride=2)
         self.act2 = nn.ReLU()
-        self.fc3 = nn.Linear(10, 640)
+        self.fc3 = nn.Linear(256, 512)
         self.act3 = nn.ReLU()
-        self.fc4 = nn.Linear(640, 3)
+        self.fc4 = nn.Linear(512, 3)
         self.act4 = nn.Sigmoid()
 
-        self.optimizer = optim.SGD(self.parameters(), lr=0.01, momentum=0.9)
+        self.loss_function = nn.BCELoss()
+        self.optimizer = optim.SGD(self.parameters(), lr=0.005, momentum=0.9)
 
     def forward(self, x):
-        x = self.act1(self.conv1(x))
-        x = self.act2(self.conv2(x))
-        # x = torch.flatten(x, 1)
+        x = self.pool(self.act1(self.conv1(x)))
+        x = self.pool(self.act2(self.conv2(x)))
+        x = torch.flatten(x, 1) #Linear needs 1 demensional vector
         x = self.act3(self.fc3(x))
         x = self.act4(self.fc4(x))
         return x
     
+    def load_trained_model(self, file):
+        self.load_state_dict(torch.load(file))
+    
     def train(self, trainset, testset, epochs, path):
         for i in range(epochs):
             for x, y in trainset:
-                print(y)
                 y_pred = self(x)
-                target = torch.tensor(y)
-                target_scaled = (target - torch.min(target))/(torch.max(target)-torch.min(target))
-                print(target_scaled)
-                loss = nn.BCELoss(y_pred, target_scaled)
                 self.optimizer.zero_grad()
+                target = torch.tensor(y).clone().detach().requires_grad_(True)
+                target_normalized = nn.functional.normalize(target)
+                print(target_normalized)
+                loss = self.loss_function(y_pred, target_normalized)
                 loss.backward()
                 self.optimizer.step()
 
             accuracy = 0
             count = 0
             for x, y in testset:
-                y_pred = self(y)
+                y_pred = self(x)
+                
                 accuracy += (torch.argmax(y_pred, 1) == y).float().sum()
                 count += len(y)
             accuracy /= count
-            print("Epoch %d: model accuracy %.2f%%" % (i, accuracy*100))
+            # print(loss)
+            # print("Epoch %d: model accuracy %.2f%%" % (i, accuracy*100))
  
-        torch.save(self.state_dict(), path)
+        torch.save(self.state_dict(), path+"trained_model.pth")
--- main.py
+++ main.py
@@ -10,25 +10,20 @@ import torchvision.transforms as transform
 def load_data():
 
     path = "data/"
-    actions = np.load(path+"actions.npy")
-    states = np.load(path+"states.npy")
+    actions = torch.from_numpy(np.load(path+"actions.npy")).to(torch.float32)
+    states = torch.from_numpy(np.load(path+"states.npy")).to(torch.float32)
 
-    x = torch.Tensor(states)
-    y = torch.Tensor(actions)
-    x = x.permute(0,3,1,2)
-
-    print(x.shape)
-
-    dataset = TensorDataset(x,y)
+    dataset = TensorDataset(states.transpose(1,3),actions)
 
     split = 0.75
     train_batch = int(split * len(dataset))
     test_batch = len(dataset) - train_batch
 
     train_dataset, test_dataset = random_split(dataset, [train_batch, test_batch])
-
-    trainloader = DataLoader(train_dataset)
-    testloader = DataLoader(test_dataset)
+    
+    batch_size = 1
+    trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
     return trainloader, testloader
@@ -46,6 +41,8 @@ def agent():
     env.close()
 
 if __name__=="__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Current device: {device}")
     train_dataset, test_dataset = load_data()
     net = cnn.Net()
     net.train(train_dataset, test_dataset, 10, "data/")
