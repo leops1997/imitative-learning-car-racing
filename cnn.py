a660ca69e1a478fbbcbb1ab84580594dfbd4c721
leops1997 <leops1997@gmail.com>
2023-06-13 19:53:17 -0300

    Refactored

--- cnn.py
+++ cnn.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional
 import torch.optim as optim
 
 
@@ -8,7 +8,8 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-                
+
+        # Build network        
         self.pool = nn.MaxPool2d(kernel_size=(2,2))
         self.conv1 = nn.Conv2d(3, 32, kernel_size=(8,8), stride=4)
         self.act1 = nn.ReLU()
@@ -19,40 +20,75 @@ class Net(nn.Module):
         self.fc4 = nn.Linear(512, 3)
         self.act4 = nn.Sigmoid()
         
-        self.loss_function = nn.BCELoss()
-        self.optimizer = optim.Adam(self.parameters(), lr=0.0001)
+        self.loss_function = nn.BCELoss() # Binary Cross Entropy
+        self.optimizer = optim.Adam(self.parameters(), lr=0.0001) # Optimization algorithm
         
+        # Compute model on selected device
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"Current device: {self.device}")
-        self.to(self.device)
+        self.to(self.device) 
 
     def forward(self, x):
-        x = x.to(self.device)
+        x = x.to(self.device) # Send data to selected device
         x = self.pool(self.act1(self.conv1(x)))
         x = self.pool(self.act2(self.conv2(x)))
-        x = torch.flatten(x, 1) #Linear needs 1 demensional vector
+        x = torch.flatten(x, 1) # Linear needs 1 demensional vector
         x = self.act3(self.fc3(x))
         x = self.act4(self.fc4(x))
         return x
     
     def train_model(self, trainset, testset, epochs, path):
+        """
+        Train model with the trainset.
+        Test model with the testset.
+        Train the model by the amount of epochs.
+        Save model on "path/trained_model.pth"
+        :param trainset: training dataset
+        :param testset: test dataset
+        :param epochs: number os epochs
+        :param path: path to save the model
+        :return cost: List with the cost of each epoch
+        :return accuracy: List with the accuracy of each epoch
+        """
+        epoch_accuracy = 0
+        epoch_loss = 0
+        accuracy = []
+        cost = []
         for i in range(epochs):
             for x, y in trainset:
-                x = x.to(torch.device(self.device))
-                y = y.to(torch.device(self.device))
-                y_pred = self(x)
-                target = torch.tensor(y).clone().detach().requires_grad_(True)
-                target_normalized = torch.sigmoid(target)
-                pred_normalized = y_pred
-                loss = self.loss_function(pred_normalized, target_normalized)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
 
+                # Send data to selected device
+                x = x.to(torch.device(self.device))
+                y = y.to(torch.device(self.device))
+
+                # Forward
+                y_pred = self(x)
+                target_normalized = torch.sigmoid(y) # Put target values between 0 and 1
+                loss = self.loss_function(y_pred, target_normalized)
+                self.optimizer.zero_grad() # Clear accumulated gradients
+
+                epoch_loss += loss.item() # Sum loss
+
+                # Backward
+                loss.backward()
+                self.optimizer.step() # Update parametes
+    
             for x, y in testset:
+                # Send data to selected device
                 x = x.to(torch.device(self.device))
                 y = y.to(torch.device(self.device))
                 y_pred = self(x)
-            print(loss)
- 
+
+                epoch_accuracy += ((torch.sigmoid(y) - y_pred) ** 2).sum().item() # Mean squared error
+
+            epoch_accuracy /= len(testset.dataset) # Mean squared error
+            epoch_loss /= len(trainset.dataset) # Total loss
+
+            accuracy.append(epoch_accuracy) # Records the accuracy for each epoch
+            cost.append(epoch_loss) # Records the loss for each epoch
+            
+            print(f"Epoch {i}: loss: {epoch_loss:.4f} - Accuracy: {epoch_accuracy:.4f}")
+
         torch.save(self.state_dict(), path+"trained_model.pth")
+
+        return cost, accuracy
--- main.py
+++ main.py
@@ -8,50 +8,63 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transform
 
 def load_data():
+    """
+    Load data from data/actions.npy and data/states.npy.
+    Transform data to tensor and create a DataLoader.
+    Split data between train data and test data.
+    """
 
+    # load data from .npy and trasform to tensor.
     path = "data/"
     actions = torch.from_numpy(np.load(path+"actions.npy")).to(torch.float32)
     states = torch.from_numpy(np.load(path+"states.npy")).to(torch.float32)
     dataset = TensorDataset(states.transpose(1,3),actions)
     
+    # split data between train data and test data.
     split = 0.75
     train_batch = int(split * len(dataset))
     test_batch = len(dataset) - train_batch
-
     train_dataset, test_dataset = random_split(dataset, [train_batch, test_batch])
+
+    # create a DataLoader.
     batch_size = 1
     trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-
     return trainloader, testloader
 
 def agent(net, track):
-    env = gym.make("CarRacing-v2", render_mode="human")
-    env.reset(seed=track) #choosing 42th map
+    """
+    Start simulation.
+    :param net: network
+    :param track: car track to play
+    """
+
+    env = gym.make("CarRacing-v2", render_mode="human") # choosing simulator
+    env.reset(seed=track) # choosing car track
     terminated = False
     action = [0, 0, 0]
-    while not terminated:
+    while not terminated: 
         env.render()
         observation, reward, terminated, truncated, info = env.step(action)
-        state = observation
-        data = torch.from_numpy(state.copy()).to(torch.float32)
+        state = observation # get state (image)
+
+        # preprocess data 
+        data = torch.from_numpy(state.copy()).to(torch.float32) 
         data = data.unsqueeze(0)
         data = data.transpose(1,3)
+
+        # get resultant action from network
         action = torch.logit(net(data)).squeeze().tolist()
-        # if action[1] <0:
-        #     action[1] = 0
-        # elif action[2] <0:
-        #     action[2] = 0  
         print(action)
     env.close()
 
 if __name__=="__main__":
-    train_dataset, test_dataset = load_data()
+    train_dataset, test_dataset = load_data() 
     net = cnn.Net()
-    net.train()
+    net.train() # training mode
     net.train_model(train_dataset, test_dataset, 20, "data/")
-    net.load_state_dict(torch.load("data/trained_model.pth"))
-    net.eval()
-    with torch.no_grad():
+    net.load_state_dict(torch.load("data/trained_model.pth")) # load trained model
+    net.eval() # evaluation mode
+    with torch.no_grad(): # disable gradient calculation for inference
         agent(net, 42)
