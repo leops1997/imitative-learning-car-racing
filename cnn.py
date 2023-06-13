f245db264851a741fe4bb6e37cdd5c5d8d270935
leops1997 <leops1997@gmail.com>
2023-06-13 00:21:00 -0300

    Need better data pre/postprocessing and use GPU

--- cnn.py
+++ cnn.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional
+import torch.nn.functional as F
 import torch.optim as optim
 
 
@@ -19,7 +19,7 @@ class Net(nn.Module):
         self.act4 = nn.Sigmoid()
 
         self.loss_function = nn.BCELoss()
-        self.optimizer = optim.SGD(self.parameters(), lr=0.005, momentum=0.9)
+        self.optimizer = optim.Adam(self.parameters(), lr=0.0001)
 
     def forward(self, x):
         x = self.pool(self.act1(self.conv1(x)))
@@ -29,18 +29,18 @@ class Net(nn.Module):
         x = self.act4(self.fc4(x))
         return x
     
-    def load_trained_model(self, file):
-        self.load_state_dict(torch.load(file))
-    
-    def train(self, trainset, testset, epochs, path):
+    def train_model(self, trainset, testset, epochs, path):
         for i in range(epochs):
             for x, y in trainset:
                 y_pred = self(x)
-                self.optimizer.zero_grad()
                 target = torch.tensor(y).clone().detach().requires_grad_(True)
-                target_normalized = nn.functional.normalize(target)
-                print(target_normalized)
-                loss = self.loss_function(y_pred, target_normalized)
+                target_normalized = torch.sigmoid(target)
+                # print(target_normalized)
+                # pred_normalized = torch.sigmoid(y_pred)
+                pred_normalized = y_pred
+                # print(torch.logit(pred_normalized))
+                loss = self.loss_function(pred_normalized, target_normalized)
+                self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
@@ -52,7 +52,7 @@ class Net(nn.Module):
                 accuracy += (torch.argmax(y_pred, 1) == y).float().sum()
                 count += len(y)
             accuracy /= count
-            # print(loss)
+            print(loss)
             # print("Epoch %d: model accuracy %.2f%%" % (i, accuracy*100))
  
         torch.save(self.state_dict(), path+"trained_model.pth")
--- main.py
+++ main.py
@@ -12,15 +12,13 @@ def load_data():
     path = "data/"
     actions = torch.from_numpy(np.load(path+"actions.npy")).to(torch.float32)
     states = torch.from_numpy(np.load(path+"states.npy")).to(torch.float32)
-
     dataset = TensorDataset(states.transpose(1,3),actions)
-
+    
     split = 0.75
     train_batch = int(split * len(dataset))
     test_batch = len(dataset) - train_batch
 
     train_dataset, test_dataset = random_split(dataset, [train_batch, test_batch])
-    
     batch_size = 1
     trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
@@ -28,16 +26,21 @@ def load_data():
 
     return trainloader, testloader
 
-def agent():
-
+def agent(net):
     env = gym.make("CarRacing-v2", render_mode="human")
-    s = env.reset(seed=42) #choosing 42th map
+    env.reset(seed=42) #choosing 42th map
     terminated = False
+    action = [0, 0, 0]
     while not terminated:
         env.render()
-        action = [0, .5, 0]
         observation, reward, terminated, truncated, info = env.step(action)
-        s = observation
+        state = observation
+        data = torch.from_numpy(state.copy()).to(torch.float32)
+        data = data.unsqueeze(0)
+        data = data.transpose(1,3)
+        action = torch.logit(net(data)).squeeze().tolist()
+
+        print(action)
     env.close()
 
 if __name__=="__main__":
@@ -45,5 +48,10 @@ if __name__=="__main__":
     print(f"Current device: {device}")
     train_dataset, test_dataset = load_data()
     net = cnn.Net()
-    net.train(train_dataset, test_dataset, 10, "data/")
-    # agent()
+    # net.to(device)
+    net.train()
+    net.train_model(train_dataset, test_dataset, 10, "data/")
+    net.load_state_dict(torch.load("data/trained_model.pth"))
+    net.eval()
+    with torch.no_grad():
+        agent(net)
