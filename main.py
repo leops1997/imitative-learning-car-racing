82f445a508af7eec483a1d4a60e73f27248f5857
shiro-nuko <pedrobene13@gmail.com>
2023-06-15 17:38:47 -0300

    Implemented plot functions

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ import sys
 import torch
 import gym
 
-if len(sys.argv) >= 4:
+if len(sys.argv) >= 5:
     epochs = int(sys.argv[1])
     batch_size = int(sys.argv[2])
     track = int(sys.argv[3])
+    should_train = bool(sys.argv[4])
 else:
     epochs = 100
     batch_size = 1
     track = 42
+    should_train = True
 
 data_path = "data/"
 
 def manage_neural_network(net):
     net.train() # Set the net to training mode
-    net.train_model(epochs, "data/")
+    cost, accuracy = net.train_model(epochs, "data/")
+    return cost, accuracy
 
 def setup_game():
     env = gym.make("CarRacing-v2", render_mode="human")
@@ -26,13 +29,34 @@ def setup_game():
     net.eval()
     return env
 
-def plot_results():
-    pass
+def plot_cost(cost):
+    plt.plot(range(epochs), cost)
+    plt.xlabel('Epochs')
+    plt.ylabel('Cost')
+    plt.title('Cost Function Value over Epochs')
+    plt.grid()
+    plt.savefig("plot_cost_function_over_epochs.png")
+    plt.show()
+    
+def plot_accuracy(accuracy):
+    plt.plot(range(epochs), accuracy)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy Value over Epochs')
+    plt.grid()
+    plt.savefig("plot_accuracy_over_epochs.png")
+    plt.show()
+
+def plot_results(cost, accuracy):
+    plot_cost(cost)
+    plt.clf()
+    plot_accuracy(accuracy)
 
 if __name__=="__main__":
     net = cnn.Net(batch_size)
-    # manage_neural_network(net)
-    plot_results()
+    if should_train:
+        cost, accuracy = manage_neural_network(net)
+        plot_results(cost, accuracy)
     env = setup_game()
     agent = imitate.Imitative_Agent(net, env, track)
     agent.play_game() # And let the agent have fun =)
